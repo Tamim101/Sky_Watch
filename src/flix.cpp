3d929ef3ec2d2f1441039c5d06c4f1c17b379a18
Tamim101 <tamimkhan7133@gmail.com>
2025-11-12 16:57:35 +0600

    add motor update

--- src/flix.cpp
+++
@@ -1,6 +0,0 @@
-void setup() {
-	Serial.begin(SERIAL_BAUDRATE);
-	print("Initializing flix\n");
-	
-	setupMotors();
-}
--- src/main.ino
+++ src/main.ino
@@ -1,18 +1,13 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include "until.h"
+float motors[4]; 
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+	Serial.begin(SERIAL_BAUDRATE);
+	print("Initializing flix\n");
+	
+	setupMotors();
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop(){
+  
 }
