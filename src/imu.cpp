4a0aca1c68dc852b4922e7007fac6cd6539fb284
Tamim101 <tamimkhan7133@gmail.com>
2025-11-15 17:12:32 +0600

    add all

--- src/imu.cpp
+++ src/imu.cpp
@@ -133,3 +133,19 @@ void printIMUInfo() {
 	print("raw gyro: %f %f %f\n", rawGyro.x, rawGyro.y, rawGyro.z);
 	print("raw acc: %f %f %f\n", rawAcc.x, rawAcc.y, rawAcc.z);
 }
+
+void printIMUInfo() {
+	imu.status() ? print("status: ERROR %d\n", imu.status()) : print("status: OK\n");
+	print("model: %s\n", imu.getModel());
+	print("who am I: 0x%02X\n", imu.whoAmI());
+	print("rate: %.0f\n", loopRate);
+	print("gyro: %f %f %f\n", rates.x, rates.y, rates.z);
+	print("acc: %f %f %f\n", acc.x, acc.y, acc.z);
+	imu.waitForData();
+	Vector rawGyro, rawAcc;
+	imu.getGyro(rawGyro.x, rawGyro.y, rawGyro.z);
+	imu.getAccel(rawAcc.x, rawAcc.y, rawAcc.z);
+	print("raw gyro: %f %f %f\n", rawGyro.x, rawGyro.y, rawGyro.z);
+	print("raw acc: %f %f %f\n", rawAcc.x, rawAcc.y, rawAcc.z);
+}
+
