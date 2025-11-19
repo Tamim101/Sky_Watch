91024ede56b4f38fe1f45357df1ef967eeb5384b
Tamim101 <tamimkhan7133@gmail.com>
2025-11-19 15:55:28 +0600

    framere were setup

--- src/main.ino
+++ src/main.ino
@@ -1,13 +1,48 @@
-#include <Arduino.h>
-#include "until.h"
-float motors[4]; 
+#include "vector.h"
+#include "quaternion.h"
+#include "util.h"
+
+#define WIFI_ENABLED 1
+
+float t = NAN; // current step time, s
+float dt; // time delta from previous step, s
+float controlRoll, controlPitch, controlYaw, controlThrottle; // pilot's inputs, range [-1, 1]
+float controlMode = NAN;
+Vector gyro; // gyroscope data
+Vector acc; // accelerometer data, m/s/s
+Vector rates; // filtered angular rates, rad/s
+Quaternion attitude; // estimated attitude
+bool landed; // are we landed and stationary
+float motors[4]; // normalized motors thrust in range [0..1]
 
 void setup() {
-	Serial.begin(SERIAL_BAUDRATE);
+	Serial.begin(115200);
 	print("Initializing flix\n");
-	
+	disableBrownOut();
+	setupParameters();
+	setupLED();
 	setupMotors();
+	setLED(true);
+#if WIFI_ENABLED
+	setupWiFi();
+#endif
+	setupIMU();
+	setupRC();
+	setLED(false);
+	print("Initializing complete\n");
 }
-void loop(){
-  
+
+void loop() {
+	readIMU();
+	step();
+	readRC();
+	estimate();
+	control();
+	sendMotors();
+	handleInput();
+#if WIFI_ENABLED
+	processMavlink();
+#endif
+	logData();
+	syncParameters();
 }
