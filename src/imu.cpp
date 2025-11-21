e445cd698d8f1bef98526de0be7b5e8b6e7259a8
Tamim101 <tamimkhan7133@gmail.com>
2025-11-21 09:42:58 +0600

    change imu code

--- src/imu.cpp
+++ src/imu.cpp
@@ -1,4 +1,3 @@
-
 // Copyright (c) 2023 Oleg Kalachev <okalachev@gmail.com>
 // Repository: https://github.com/okalachev/flix
 
@@ -6,133 +5,155 @@
 
 #include <SPI.h>
 #include <FlixPeriph.h>
+#include <Wire.h>
 #include "vector.h"
 #include "lpf.h"
 #include "util.h"
 
-MPU9250 imu(SPI);
+#define MPU6050_ADDR 0x68
+
+extern Vector acc;
+extern Vector gyro;
 
 Vector accBias;
-Vector accScale(1, 1, 1);
+Vector accScale(1,1,1);
 Vector gyroBias;
 
-void setupIMU() {
-	print("Setup IMU\n");
-	imu.begin();
-	configureIMU();
+void writeReg(uint8_t reg, uint8_t val) {
+    Wire.beginTransmission(MPU6050_ADDR);
+    Wire.write(reg);
+    Wire.write(val);
+    Wire.endTransmission();
 }
 
-void configureIMU() {
-	imu.setAccelRange(imu.ACCEL_RANGE_4G);
-	imu.setGyroRange(imu.GYRO_RANGE_2000DPS);
-	imu.setDLPF(imu.DLPF_MAX);
-	imu.setRate(imu.RATE_1KHZ_APPROX);
-	imu.setupInterrupt();
+void readRegs(uint8_t reg, uint8_t *buf, uint8_t len) {
+    Wire.beginTransmission(MPU6050_ADDR);
+    Wire.write(reg);
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU6050_ADDR, len, true);
+    for (int i = 0; i < len; i++) buf[i] = Wire.read();
+}
+
+void setupIMU() {
+    print("Setup MPU-6050\n");
+    Wire.begin();
+    delay(100);
+
+    // Wake up
+    writeReg(0x6B, 0x00);
+
+    // Gyro ±2000 dps
+    writeReg(0x1B, 0x18);
+
+    // Acc ±4G (same as Flix)
+    writeReg(0x1C, 0x08);
+
+    // DLPF: 42 Hz (good for small drones)
+    writeReg(0x1A, 0x03);
+
+    // Sample rate = 1 kHz / (1 + 0)
+    writeReg(0x19, 0x00);
+
+    print("MPU-6050 OK\n");
 }
 
 void readIMU() {
-	imu.waitForData();
-	imu.getGyro(gyro.x, gyro.y, gyro.z);
-	imu.getAccel(acc.x, acc.y, acc.z);
-	calibrateGyroOnce();
-	// apply scale and bias
-	acc = (acc - accBias) / accScale;
-	gyro = gyro - gyroBias;
-	// rotate
-	rotateIMU(acc);
-	rotateIMU(gyro);
-}
+    uint8_t buf[14];
+    readRegs(0x3B, buf, 14);
 
-void rotateIMU(Vector& data) {
-	// Rotate from LFD to FLU
-	// NOTE: In case of using other IMU orientation, change this line:
-	data = Vector(data.y, data.x, -data.z);
-	// Axes orientation for various boards: https://github.com/okalachev/flixperiph#imu-axes-orientation
+    int16_t ax = (buf[0] << 8) | buf[1];
+    int16_t ay = (buf[2] << 8) | buf[3];
+    int16_t az = (buf[4] << 8) | buf[5];
+    int16_t gx = (buf[8] << 8) | buf[9];
+    int16_t gy = (buf[10] << 8) | buf[11];
+    int16_t gz = (buf[12] << 8) | buf[13];
+
+    // Convert to real units
+    acc.x = ax / 8192.0;   // ±4G
+    acc.y = ay / 8192.0;
+    acc.z = az / 8192.0;
+
+    gyro.x = gx / 16.4;    // ±2000 dps
+    gyro.y = gy / 16.4;
+    gyro.z = gz / 16.4;
+
+    calibrateGyroOnce();
+
+    // Apply calibration
+    acc = (acc - accBias) / accScale;
+    gyro = gyro - gyroBias;
+
+    // Orientation fix (same as Flix)
+    acc = Vector(acc.y, acc.x, -acc.z);
+    gyro = Vector(gyro.y, gyro.x, -gyro.z);
 }
 
 void calibrateGyroOnce() {
-	static Delay landedDelay(2);
-	if (!landedDelay.update(landed)) return; // calibrate only if definitely stationary
+    static Delay landedDelay(2);
+    if (!landedDelay.update(landed)) return;
 
-	static LowPassFilter<Vector> gyroCalibrationFilter(0.001);
-	gyroBias = gyroCalibrationFilter.update(gyro);
+    static LowPassFilter<Vector> gyroCalibrationFilter(0.001);
+    gyroBias = gyroCalibrationFilter.update(gyro);
 }
 
 void calibrateAccel() {
-	print("Calibrating accelerometer\n");
-	imu.setAccelRange(imu.ACCEL_RANGE_2G); // the most sensitive mode
+    print("Calibrating MPU-6050 accel…\n");
 
-	print("1/6 Place level [8 sec]\n");
-	pause(8);
-	calibrateAccelOnce();
-	print("2/6 Place nose up [8 sec]\n");
-	pause(8);
-	calibrateAccelOnce();
-	print("3/6 Place nose down [8 sec]\n");
-	pause(8);
-	calibrateAccelOnce();
-	print("4/6 Place on right side [8 sec]\n");
-	pause(8);
-	calibrateAccelOnce();
-	print("5/6 Place on left side [8 sec]\n");
-	pause(8);
-	calibrateAccelOnce();
-	print("6/6 Place upside down [8 sec]\n");
-	pause(8);
-	calibrateAccelOnce();
+    Vector accMax(-INFINITY, -INFINITY, -INFINITY);
+    Vector accMin(INFINITY, INFINITY, INFINITY);
 
-	printIMUCalibration();
-	print("✓ Calibration done!\n");
-	configureIMU();
-}
+    for (int face = 1; face <= 6; face++) {
+        print("%d/6 hold still [8 sec]\n", face);
+        pause(8);
 
-void calibrateAccelOnce() {
-	const int samples = 1000;
-	static Vector accMax(-INFINITY, -INFINITY, -INFINITY);
-	static Vector accMin(INFINITY, INFINITY, INFINITY);
+        Vector avg(0,0,0);
+        const int samples = 1000;
 
-	// Compute the average of the accelerometer readings
-	acc = Vector(0, 0, 0);
-	for (int i = 0; i < samples; i++) {
-		imu.waitForData();
-		Vector sample;
-		imu.getAccel(sample.x, sample.y, sample.z);
-		acc = acc + sample;
-	}
-	acc = acc / samples;
+        for (int i = 0; i < samples; i++) {
+            uint8_t buf[6];
+            readRegs(0x3B, buf, 6);
+            int16_t ax = (buf[0] << 8) | buf[1];
+            int16_t ay = (buf[2] << 8) | buf[3];
+            int16_t az = (buf[4] << 8) | buf[5];
+            avg.x += ax;
+            avg.y += ay;
+            avg.z += az;
+        }
 
-	// Update the maximum and minimum values
-	if (acc.x > accMax.x) accMax.x = acc.x;
-	if (acc.y > accMax.y) accMax.y = acc.y;
-	if (acc.z > accMax.z) accMax.z = acc.z;
-	if (acc.x < accMin.x) accMin.x = acc.x;
-	if (acc.y < accMin.y) accMin.y = acc.y;
-	if (acc.z < accMin.z) accMin.z = acc.z;
-	// Compute scale and bias
-	accScale = (accMax - accMin) / 2 / ONE_G;
-	accBias = (accMax + accMin) / 2;
+        avg = avg / samples / 8192.0; // convert to G
+
+        accMax.x = max(accMax.x, avg.x);
+        accMax.y = max(accMax.y, avg.y);
+        accMax.z = max(accMax.z, avg.z);
+        accMin.x = min(accMin.x, avg.x);
+        accMin.y = min(accMin.y, avg.y);
+        accMin.z = min(accMin.z, avg.z);
+    }
+
+    accScale = (accMax - accMin) / 2.0 / ONE_G;
+    accBias = (accMax + accMin) / 2.0;
+
+    print("Accel calibration done!\n");
+    printIMUCalibration();
 }
 
 void printIMUCalibration() {
-	print("gyro bias: %f %f %f\n", gyroBias.x, gyroBias.y, gyroBias.z);
-	print("accel bias: %f %f %f\n", accBias.x, accBias.y, accBias.z);
-	print("accel scale: %f %f %f\n", accScale.x, accScale.y, accScale.z);
+    print("gyro bias: %f %f %f\n", gyroBias.x, gyroBias.y, gyroBias.z);
+    print("accel bias: %f %f %f\n", accBias.x, accBias.y, accBias.z);
+    print("accel scale: %f %f %f\n", accScale.x, accScale.y, accScale.z);
 }
+
+
 
 void printIMUInfo() {
-	imu.status() ? print("status: ERROR %d\n", imu.status()) : print("status: OK\n");
-	print("model: %s\n", imu.getModel());
-	print("who am I: 0x%02X\n", imu.whoAmI());
-	print("rate: %.0f\n", loopRate);
-	print("gyro: %f %f %f\n", rates.x, rates.y, rates.z);
-	print("acc: %f %f %f\n", acc.x, acc.y, acc.z);
-	imu.waitForData();
-	Vector rawGyro, rawAcc;
-	imu.getGyro(rawGyro.x, rawGyro.y, rawGyro.z);
-	imu.getAccel(rawAcc.x, rawAcc.y, rawAcc.z);
-	print("raw gyro: %f %f %f\n", rawGyro.x, rawGyro.y, rawGyro.z);
-	print("raw acc: %f %f %f\n", rawAcc.x, rawAcc.y, rawAcc.z);
+    // WHO_AM_I register for MPU6050 = 0x75
+    uint8_t whoami;
+    readRegs(0x75, &whoami, 1);
+
+    print("status: OK\n");
+    print("model: MPU-6050\n");
+    print("who am I: 0x%02X\n", whoami);
+    print("rate: %.0f\n", loopRate);
+    print("gyro: %f %f %f\n", gyro.x, gyro.y, gyro.z);
+    print("acc: %f %f %f\n", acc.x, acc.y, acc.z);
 }
-
-
-
