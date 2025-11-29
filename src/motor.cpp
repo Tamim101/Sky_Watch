04d3b8d87ae1b03f36b8878409f91dcc28614b30
Tamim101 <tamimkhan7133@gmail.com>
2025-11-29 20:34:45 +0600

    add something

--- src/motor.cpp
+++ src/motor.cpp
@@ -60,3 +60,14 @@ void testMotor(int n) {
 	sendMotors();
 	print("Done\n");
 }
+
+void testMotor(int n) {
+	print("Testing motor %d\n", n);
+	motors[n] = 1;
+	delay(50); // ESP32 may need to wait until the end of the current cycle to change duty https://github.com/espressif/arduino-esp32/issues/5306
+	sendMotors();
+	pause(3);
+	motors[n] = 0;
+	sendMotors();
+	print("Done\n");
+}
