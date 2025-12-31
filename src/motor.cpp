43c2bce22844161c50e05711d0a5e6867e84a5a6
Tamim101 <tamimkhan7133@gmail.com>
2025-12-31 19:15:35 +0600

    add

--- src/motor.cpp
+++ src/motor.cpp
@@ -4,12 +4,12 @@
 
 #define MOTOR_0_PIN 12  // rear left
 #define MOTOR_1_PIN 13  // rear right
-#define MOTOR_2_PIN 14   // front right
+#define MOTOR_2_PIN 16   // front right
 #define MOTOR_3_PIN 15   // front left
 
 // Good combo for ESP32-C3 + brushed motors
 #define PWM_FREQUENCY   78000           // 16 kHz (quiet, safe)
-#define PWM_RESOLUTION  10               // 8-bit (0..255)
+#define PWM_RESOLUTION  16             // 8-bit (0..255)
 
 // PWM in microseconds equivalent (period = 1e6 / freq)
 #define PWM_STOP        0
