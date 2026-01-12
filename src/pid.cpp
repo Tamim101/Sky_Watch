c032d67247398ea61bc5f9354cf6c23e27a5bd63
Tamim101 <tamimkhan7133@gmail.com>
2026-01-12 19:12:08 +0600

    remove

--- src/pid.cpp
+++ src/pid.cpp
@@ -9,12 +9,3 @@ float pid_step(PID &pid, float error, float dt)
 
     return pid.kp * error + pid.i_term + pid.kd * d;
 }
-float pid_step(PID &pid, float error, float dt)
-{
-    pid.i_term += error * pid.ki * dt;
-
-    float d = (error - pid.last_error) / dt;
-    pid.last_error = error;
-
-    return pid.kp * error + pid.i_term + pid.kd * d;
-}
